d6521c0ceb1c8b27c0013d0541a6a871012b25e8
tyler <tylerknapp25@gmail.com>
2022-02-22 21:57:08 -0500

    verified post request via postman

--- src/main/java/com/philodelight/authdemo/registration/RegistrationController.java
+++ src/main/java/com/philodelight/authdemo/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.philodelight.authdemo.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class RegistrationController {
     
     private RegistrationService registrationService;
     
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
--- src/main/java/com/philodelight/authdemo/registration/RegistrationService.java
+++ src/main/java/com/philodelight/authdemo/registration/RegistrationService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
-        return "works";
+        return "it works";
     }
 }
